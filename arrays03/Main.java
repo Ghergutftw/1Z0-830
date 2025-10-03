aab74bb185ea42d0f6fe7ff123f59322cb35a1cb
GherMa <madalinghergut@gmail.com>
2025-10-03 18:41:44 +0300

    arrrays soring

--- arrays03/Main.java
+++ arrays03/Main.java
@@ -30,12 +30,28 @@ public class Main {
 
         char[] charArray = new char[] { 'z', 'B', 'A', 'd', 'C', 'x' };
         Arrays.sort(charArray);
-        System.out.println("Sortare alfabetica");
-        System.out.println(Arrays.toString(charArray));
+        System.out.println("Sortare alfabetica char");
+        System.out.println(Arrays.toString(charArray)); // [A, B, C, d, x, z]
+                                                        // Se ia dupa codul ASCII
 
         Arrays.sort(array3);
         System.out.println("Sortare numerica");
         System.out.println(Arrays.toString(array3));
+
+        System.out.println("Sortare string cu numere");
+        String[] stringArray = new String[] { "3", "300", "20", "35", "37", "4", "70" };
+        Arrays.sort(stringArray);
+        System.out.println(Arrays.toString(stringArray));
+
+        System.out.println("Sortare String cu Cuvinte");
+        String[] stringArrays2 = new String[] { "Cow", "Bull", "cat", "deer" };
+        Arrays.sort(stringArrays2);
+        System.out.println(Arrays.toString(stringArrays2));
+
+        System.out.println("Sortare String cu Cuvinte + Cifre");
+        String[] stringArrays3 = new String[] { "Cow", "Bull", "cat", "deer", "123", "300", "12" };
+        Arrays.sort(stringArrays3);
+        System.out.println(Arrays.toString(stringArrays3));
 
     }
 }
