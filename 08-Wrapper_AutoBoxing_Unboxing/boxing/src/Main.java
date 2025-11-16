4d21b138b6a539cad5206c3f3462748285a75479
GherMa <madalinghergut@gmail.com>
2025-11-16 19:34:39 +0200

    I am not a wrapper

---
+++ 08-Wrapper_AutoBoxing_Unboxing/boxing/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+
+        // autoboxing
+        Double db = 12.6;
+        double d = db;
+
+        System.out.println(db);
+        System.out.println(d);
+
+        Integer in = 3;
+        int i = in;
+
+        int a = (Integer) null;
+    }
+}
