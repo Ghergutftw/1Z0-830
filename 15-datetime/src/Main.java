f50b73ebf8b3f72b7fe955425021bd8e20c76190
GherMa <madalinghergut@gmail.com>
2026-03-07 01:16:53 +0200

    Start Period

--- 15-datetime/src/Main.java
+++ 15-datetime/src/Main.java
@@ -1,11 +1,14 @@
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
 
 public class Main{
     public static void main(String[] args) {
-        calendarClass();
+        period();
     }
 
     static void calendarClass(){
@@ -48,5 +51,108 @@ public class Main{
 
         //Calendar Class is mutable
         System.out.println("YEAR: " + birthDate.get(Calendar.YEAR));
+        birthDate.add(Calendar.YEAR, 22);
+        System.out.println("22 years later : " + birthDate.get(Calendar.YEAR));
+    }
+
+    static void localDate(){
+        LocalDate date = LocalDate.now();
+        System.out.println("Current Date: " + date);
+        int year = date.getYear();
+        int monthValue = date.getMonthValue();
+        int dayOfYear = date.getDayOfMonth();
+        System.out.println(dayOfYear + "-" + monthValue + "-" + year);
+        Month month = date.getMonth();
+        System.out.println("Current month: " + month);
+        DayOfWeek dayOfWeek =  date.getDayOfWeek();
+        System.out.println("Day of the week: " + dayOfWeek);
+
+        Calendar calendar = Calendar.getInstance();
+        System.out.println("Day of the week in Calendar : " + calendar.get(Calendar.DAY_OF_WEEK));
+
+        LocalDate threeWeeksAgo = date.minusWeeks(3);
+        System.out.println("3 weeks ago: " + threeWeeksAgo);
+
+        LocalDate fiveDaysLater = date.plusDays(5);
+        System.out.println("5 days later: " + fiveDaysLater);
+
+        //localDate class is immutable
+        System.out.println("Current date: " + date);
+        date.plusDays(10);//Current date: 2026-03-06
+        date = date.plusDays(10);
+        System.out.println("Current date: " + date);//Current date: 2026-03-16
+
+        LocalDate weddingDate = LocalDate.of(2032, Month.APRIL, 06);
+        System.out.println("Wedding Date: " + weddingDate );
+
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        System.out.println("Formatted : " + format.format(weddingDate));
+
+        System.out.println("is wedding date after current date ? " + (weddingDate.isBefore(date) ? "Da" : "Nu"));
+
+
+    }
+
+    static void localTime(){
+        LocalTime time = LocalTime.now();
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("hh:mm:ss");
+
+        System.out.println("Current time: " + time);
+
+        int hour = time.getHour();
+        int minute = time.getMinute();
+        int second = time.getSecond();
+        int nanosecond = time.getNano();
+
+        System.out.println("Current hour: " +hour);
+        System.out.println("Current minute: " + minute);
+        System.out.println("Current second: " + second);
+        System.out.println("Current nanosecond: " + nanosecond);
+
+        LocalTime twoHoursAgo = time.minusHours(2);
+        System.out.println("2 hours ago: " + twoHoursAgo);
+
+        LocalTime thirtyMinutesLater = time.plusMinutes(30);
+        System.out.println("30 minutes later: " + format.format(thirtyMinutesLater));
+
+        System.out.println("Immutable: " + time);
+
+        LocalTime appointmentTime = LocalTime.of(15, 30 , 0);
+        System.out.println("Appointment Time: " + appointmentTime);
+
+        appointmentTime.isBefore(LocalTime.now());
+
+
+
+    }
+
+    static void localDateTime(){
+        LocalDateTime dateTime = LocalDateTime.now();
+        System.out.println("Current Date Time" + dateTime);
+
+        int year = dateTime.getYear();
+        int second = dateTime.getSecond();
+        System.out.println("Current year: " + year);
+        System.out.println("Current second: " + second);
+
+        LocalDateTime mixedTime = dateTime.plusDays(3).minusMonths(2);
+        System.out.println("Mixed time: " + mixedTime);
+
+        LocalDateTime birthday = LocalDateTime.of(2004,12, 6, 23, 30,0);
+        System.out.println("Birthday " + birthday);
+
+        System.out.println("is my birthday before current date time ? " + birthday.isBefore(dateTime));
+
+    }
+
+    static void period(){
+        Period period = Period.of(2,04,0);
+        System.out.println("Period : " + period);
+
+        LocalDate currentDate = LocalDate.now();
+        System.out.println("Current Date  " + currentDate);
+
+        LocalDate nextDate = currentDate.plus(period);
+        System.out.println("Next Date " + nextDate);
     }
 }
